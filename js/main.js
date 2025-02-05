faa7cf2125dbc15678b55ffa7c24c1bfb5b4565c
noahk3409 <113371785+noahk3409@users.noreply.github.com>
2025-02-05 15:22:18 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 console.log("JS file connected");
 
+// Get each svg image by their id.
+const chocolateImage = document.getElementById("chocolate");
+const strawberryImage = document.getElementById("strawberry");
+const bananaImage = document.getElementById("banana");
+
+// If Chocolate Image Exists, add a click event listener to it that will print to console.
+if(chocolateImage){
+    chocolateImage.addEventListener("click", (event) => {
+        console.log("Chocolate was clicked!")
+    });
+}
+
+// If Strawberry Image Exists, add a click event listener to it that will print to console.
+if(strawberryImage){
+    strawberryImage.addEventListener("click", (event) => {
+        console.log("Strawberry was clicked!")
+    });
+}
+
+// If Banana Image Exists, add a click event listener to it that will print to console.
+if(bananaImage){
+    bananaImage.addEventListener("click", (event) => {
+        console.log("Banana was clicked!")
+    });
+}
